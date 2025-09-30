c9ccfe4442846456a60da28c83cc15bd09fa9277
Eleazar Resendez <e.resendez@outlook.com>
2025-09-30 15:46:40 -0600

    Add TopNavbar and GuestLayout components; refactor Welcome page to use GuestLayout

---
+++ resources/js/layouts/guest/GuestSimpleLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TopNavbar from '@/components/TopNavbar.vue';
+</script>
+
+<template>
+    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
+        <TopNavbar />
+        <slot />
+    </div>
+</template>
