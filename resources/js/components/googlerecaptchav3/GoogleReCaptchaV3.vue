a1408a38be333a1cc6e8ecc4d223e9bc121e0880
Eleazar Resendez <e.resendez@outlook.com>
2025-10-11 10:47:37 -0600

    Refactor GoogleReCaptchaV3 component to use script setup and defineProps for improved readability and maintainability

--- resources/js/components/googlerecaptchav3/GoogleReCaptchaV3.vue
+++ resources/js/components/googlerecaptchav3/GoogleReCaptchaV3.vue
@@ -1,85 +1,172 @@
 <template>
-  <div :id="id"></div>
+  <div :id="props.id"></div>
 </template>
 
-<script>
-export default {
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+defineOptions({
   name: 'google-recaptcha-v3',
-  props: {
-    action: {
-      type: String,
-      required: false,
-      default: 'validate_grecaptcha',
-    },
-    id: {
-      type: String,
-      required: false,
-      default: 'grecaptcha_container',
-    },
-    siteKey: {
-      type: String,
-      required: false, // set to true if you don't want to store the siteKey in this component
-      default: '', // set siteKey here if you want to store it in this component
-    },
-    inline: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      captchaId: null,
-    };
-  },
-  mounted() {
-    this.init();
-  },
-  methods: {
-    init() {
-      if (!document.getElementById('gRecaptchaScript')) {
-        window.gRecaptchaOnLoadCallbacks = [this.render];
-        window.gRecaptchaOnLoad = function () {
-          for (let i = 0; i < window.gRecaptchaOnLoadCallbacks.length; i++) {
-            window.gRecaptchaOnLoadCallbacks[i]();
-          }
-          delete window.gRecaptchaOnLoadCallbacks;
-          delete window.gRecaptchaOnLoad;
-        };
+});
 
-        const recaptchaScript = document.createElement('script');
-        recaptchaScript.setAttribute('src', 'https://www.google.com/recaptcha/api.js?render=explicit&onload=gRecaptchaOnLoad');
-        recaptchaScript.setAttribute('id', 'gRecaptchaScript');
-        recaptchaScript.async = true;
-        recaptchaScript.defer = true;
-        document.head.appendChild(recaptchaScript);
-      } else if (!window.grecaptcha || !window.grecaptcha.render) {
-        window.gRecaptchaOnLoadCallbacks.push(this.render);
-      } else {
-        this.render();
-      }
-    },
-
-    render() {
-      this.captchaId = window.grecaptcha.render(this.id, {
-        sitekey: this.siteKey,
-        badge: this.inline === true ? 'inline' : '',
-        size: 'invisible',
-        'expired-callback': this.execute,
-      });
-
-      this.execute();
-    },
-
-    execute() {
-      window.grecaptcha
-        .execute(this.captchaId, {
-          action: this.action,
-        })
-        .then((token) => {
-          this.$emit('input', token);
-        });
-    },
+const props = withDefaults(
+  defineProps<{
+    action?: string;
+    id?: string;
+    siteKey?: string;
+    inline?: boolean;
+  }>(),
+  {
+    action: 'validate_grecaptcha',
+    id: 'grecaptcha_container',
+    siteKey: '',
+    inline: false,
   },
+);
+
+const emit = defineEmits<{
+  (event: 'input', token: string): void;
+}>();
+
+type RecaptchaCallback = () => void;
+
+type GrecaptchaRenderOptions = {
+  sitekey: string;
+  badge?: string;
+  size?: 'invisible' | 'compact' | 'normal';
+  'expired-callback'?: () => void;
 };
+
+interface Grecaptcha {
+  render(container: string | HTMLElement, parameters: GrecaptchaRenderOptions): number;
+  execute(widgetId: number, options: { action: string }): Promise<string>;
+}
+
+interface RecaptchaWindow extends Window {
+  grecaptcha?: Grecaptcha;
+  gRecaptchaOnLoadCallbacks?: RecaptchaCallback[];
+  gRecaptchaOnLoad?: () => void;
+}
+
+const isClient = typeof window !== 'undefined' && typeof document !== 'undefined';
+const RECAPTCHA_SCRIPT_ID = 'gRecaptchaScript';
+
+const captchaId = ref<number | null>(null);
+
+const executeCaptcha = () => {
+  if (!isClient) {
+    return;
+  }
+
+  const win = window as RecaptchaWindow;
+
+  if (captchaId.value === null || !win.grecaptcha?.execute) {
+    return;
+  }
+
+  win.grecaptcha
+    .execute(captchaId.value, {
+      action: props.action,
+    })
+    .then((token) => {
+      emit('input', token);
+    });
+};
+
+const renderCaptcha = () => {
+  if (!isClient) {
+    return;
+  }
+
+  const win = window as RecaptchaWindow;
+
+  if (!win.grecaptcha?.render) {
+    return;
+  }
+
+  captchaId.value = win.grecaptcha.render(props.id, {
+    sitekey: props.siteKey,
+    badge: props.inline ? 'inline' : '',
+    size: 'invisible',
+    'expired-callback': executeCaptcha,
+  });
+
+  executeCaptcha();
+};
+
+const registerOnLoadCallback = () => {
+  if (!isClient) {
+    return;
+  }
+
+  const win = window as RecaptchaWindow;
+
+  if (!win.gRecaptchaOnLoadCallbacks) {
+    win.gRecaptchaOnLoadCallbacks = [];
+  }
+
+  if (!win.gRecaptchaOnLoadCallbacks.includes(renderCaptcha)) {
+    win.gRecaptchaOnLoadCallbacks.push(renderCaptcha);
+  }
+};
+
+const init = () => {
+  if (!isClient) {
+    return;
+  }
+
+  const win = window as RecaptchaWindow;
+  const scriptExists = Boolean(document.getElementById(RECAPTCHA_SCRIPT_ID));
+
+  if (!scriptExists) {
+    registerOnLoadCallback();
+
+    if (!win.gRecaptchaOnLoad) {
+      win.gRecaptchaOnLoad = () => {
+        win.gRecaptchaOnLoadCallbacks?.forEach((callback) => {
+          callback();
+        });
+        delete win.gRecaptchaOnLoadCallbacks;
+        delete win.gRecaptchaOnLoad;
+      };
+    }
+
+    const recaptchaScript = document.createElement('script');
+    recaptchaScript.setAttribute('src', 'https://www.google.com/recaptcha/api.js?render=explicit&onload=gRecaptchaOnLoad');
+    recaptchaScript.setAttribute('id', RECAPTCHA_SCRIPT_ID);
+    recaptchaScript.async = true;
+    recaptchaScript.defer = true;
+    document.head.appendChild(recaptchaScript);
+    return;
+  }
+
+  if (!win.grecaptcha || !win.grecaptcha.render) {
+    registerOnLoadCallback();
+    return;
+  }
+
+  renderCaptcha();
+};
+
+onMounted(() => {
+  init();
+});
+
+onBeforeUnmount(() => {
+  if (!isClient) {
+    return;
+  }
+
+  const win = window as RecaptchaWindow;
+
+  if (!win.gRecaptchaOnLoadCallbacks) {
+    return;
+  }
+
+  win.gRecaptchaOnLoadCallbacks = win.gRecaptchaOnLoadCallbacks.filter((callback) => callback !== renderCaptcha);
+
+  if (win.gRecaptchaOnLoadCallbacks.length === 0) {
+    delete win.gRecaptchaOnLoadCallbacks;
+  }
+});
 </script>
